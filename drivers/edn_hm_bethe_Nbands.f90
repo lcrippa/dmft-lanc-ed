46503d8eba56f80240566c2fbcedece657be345c
Adriano Amaricci <adriano.amaricci@gmail.com>
2018-08-07 12:41:55 +0200

    v0.7.1 UPDATE:
    Code update to deal with 5bands.
    A DBLE PRECISION version is available on a separate branch (waiting to merge).

--- drivers/edn_hm_bethe_Nbands.f90
+++ drivers/edn_hm_bethe_Nbands.f90
@@ -4,7 +4,7 @@ program hm_Nbands_bethe
   USE DMFT_TOOLS
   USE MPI
   implicit none
-  integer                                     :: iloop,Nb,Le,Nso
+  integer                                     :: iloop,Nb,Le,Nso,iorb
   logical                                     :: converged
   real(8),dimension(5)                        :: Wbethe,Dbethe
   !Bath:
@@ -20,17 +20,26 @@ program hm_Nbands_bethe
   !
   complex(8),allocatable,dimension(:,:,:,:,:) :: Weiss,Smats,Sreal,Gmats,Greal
   character(len=16)                           :: finput
-  real(8)                                     :: wmixing,Eout(2)
-  character(len=10)                           :: dos_model
+  real(8)                                     :: wmixing
+  !
+  integer                                     :: comm,rank
+  logical                                     :: master
+
+  call init_MPI()
+  comm = MPI_COMM_WORLD
+  call StartMsg_MPI(comm)
+  rank = get_Rank_MPI(comm)
+  master = get_Master_MPI(comm)
+
 
 
   call parse_cmd_variable(finput,"FINPUT",default='inputED.in')
   call parse_input_variable(Le,"LE",finput,default=500)
   call parse_input_variable(Wbethe,"WBETHE",finput,default=[1d0,1d0,1d0,1d0,1d0])
-  call parse_input_variable(Dbethe,"DBETHE",finput,default=[-1d0,-0.5d0,0d0,0.5d0,1d0])
+  call parse_input_variable(Dbethe,"DBETHE",finput,default=[0d0,0d0,0d0,0d0,0d0])
   call parse_input_variable(wmixing,"WMIXING",finput,default=0.5d0)
   !
-  call ed_read_input(trim(finput))
+  call ed_read_input(trim(finput),comm)
 
   !Add DMFT CTRL Variables:
   call add_ctrl_var(Norb,"norb")
@@ -57,7 +66,7 @@ program hm_Nbands_bethe
      Ebands(iorb,:) = linspace(-Wband(iorb),Wband(iorb),Le,mesh=de(iorb))
      Dbands(iorb,:) = dens_bethe(Ebands(iorb,:),Wband(iorb))*de(iorb)
   enddo
-  call TB_write_Hloc(one*diag(H0))
+  if(master)call TB_write_Hloc(one*diag(H0))
 
 
   !Allocate Weiss Field:
@@ -74,65 +83,54 @@ program hm_Nbands_bethe
   Nb=get_bath_dimension()
   allocate(bath(Nb))
   allocate(bath_(Nb))
-  call ed_init_solver(bath,Hloc)
+  call ed_init_solver(comm,bath,Hloc)
 
 
   !DMFT loop
   iloop=0;converged=.false.
   do while(.not.converged.AND.iloop<nloop)
      iloop=iloop+1
-     call start_loop(iloop,nloop,"DMFT-loop")
+     if(master)call start_loop(iloop,nloop,"DMFT-loop")
      !
      !Solve the EFFECTIVE IMPURITY PROBLEM (first w/ a guess for the bath)
-     call ed_solve(bath)
+     call ed_solve(comm,bath)
      call ed_get_sigma_matsubara(Smats)
      call ed_get_sigma_real(Sreal)
      !
      ! compute the local gf:
-     call dmft_gloc_matsubara(Ebands,Dbands,H0,Gmats,Smats)
+     call dmft_gloc_matsubara(comm,Ebands,Dbands,H0,Gmats,Smats)
+     if(master)call dmft_print_gf_matsubara(Gmats,"Gloc",iprint=1)
      !
      !Get the Weiss field/Delta function to be fitted
-     if(cg_scheme=='weiss')then
-        call dmft_weiss(Gmats,Smats,Weiss,Hloc,iprint=1)
-     else
-        call dmft_delta(Gmats,Smats,Weiss,Hloc,iprint=1)
-     endif
+     call dmft_self_consistency(comm,Gmats,Smats,Weiss,Hloc,SCtype=cg_scheme)
+     ! if(master)then
+     !    if(cg_scheme=='weiss')then
+     !       call dmft_weiss(Gmats,Smats,Weiss,Hloc)
+     !    else
+     !       call dmft_delta(Gmats,Smats,Weiss,Hloc)
+     !    endif
+     ! endif
+     ! call Bcast_MPI(comm,Weiss)
      !
      !
      !Perform the SELF-CONSISTENCY by fitting the new bath
-     call ed_chi2_fitgf(Weiss,bath,ispin=1)
+     call ed_chi2_fitgf(comm,Weiss,bath,ispin=1)
      !
      !MIXING:
      if(iloop>1)Bath = wmixing*Bath + (1.d0-wmixing)*Bath_
      Bath_=Bath
 
      !Check convergence (if required change chemical potential)
-     converged = check_convergence(Weiss(1,1,1,1,:),dmft_error,nsuccess,nloop,reset=.false.)
+     if(master)converged = check_convergence(Weiss(1,1,:,:,:),dmft_error,nsuccess,nloop,reset=.false.)
+     call Bcast_MPI(comm,converged)
      !
      call end_loop
   enddo
 
 
-  call dmft_gloc_realaxis(Ebands,Dbands,H0,Greal,Sreal,iprint=1)
-  Eout = dmft_kinetic_energy(Ebands,Dbands,H0,Smats(1,1,:,:,:))
-
-
-contains
-
-
-
-  function dens_flat(ebands,wband) result(dens)
-    real(8),dimension(:)            :: ebands
-    real(8)                         :: wband
-    real(8),dimension(size(ebands)) :: dens
-    integer                         :: i
-    real(8)                         :: e
-    do i=1,size(ebands)
-       e=ebands(i)
-       dens(i)= step(wband-abs(e))/(2*wband)
-    enddo
-  end function dens_flat
-
+  call dmft_gloc_realaxis(Comm,Ebands,Dbands,H0,Greal,Sreal)
+  if(master)call dmft_print_gf_realaxis(Greal,"Greal",iprint=1)
+  call dmft_kinetic_energy(comm,Ebands,Dbands,H0,Smats(1,1,:,:,:))
 
 
 end program hm_Nbands_bethe
